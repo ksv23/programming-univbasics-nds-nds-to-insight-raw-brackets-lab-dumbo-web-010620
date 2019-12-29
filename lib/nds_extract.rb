ce3944178d11ba4051d653dfe66101a4a8abdf8f
Kring Villanueva <ksvillanueva7@gmail.com>
2019-12-29 04:43:37 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -30,7 +30,7 @@ def directors_totals(nds)
   while start < nds.length do
       total = 0
       movie_count = 0
-      while movies < nds[start][:movies].length
+      while movie_count < nds[start][:movies].length
         total += nds[start][:movies][movie_count][:worldwide_gross]
         movie_count += 1
       end
